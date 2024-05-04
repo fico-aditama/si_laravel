56635a5f0f471223c1ecb8058f0760328e6396b0
Fiko Aditama <fiko@simise.id>
2024-05-04 15:44:58 +0700

    Initiate Project

--- gitpush.sh
+++ gitpush.sh
@@ -2,5 +2,5 @@ git init .
 git add .
 git commit -m "Initiate Project"
 git branch -M main
-git remote add origin https://github.com/fico-aditama/si_laravel.git
+git remote add origin git@github.com:fico-aditama/si_laravel.git
 git push -u origin main
