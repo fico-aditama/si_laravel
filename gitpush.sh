aa20c68bbb82e25d6558896368370c9327938e3f
Fiko Aditama <fiko@simise.id>
2024-05-07 01:19:53 +0700

    Implementasi SB Admin

--- gitpush.sh
+++ gitpush.sh
@@ -1,6 +1,6 @@
 git init .
 git add .
-git commit -m "Sesi 8"
+git commit -m "Implementasi SB Admin"
 git branch -M main
 git remote add origin1 git@github.com:fico-aditama/si_laravel.git
 git push -u origin main
