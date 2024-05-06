392862040987da557a0e2d52fcd908110a965e0d
Fiko Aditama <fiko@simise.id>
2024-05-07 01:17:25 +0700

    Sesi 8

--- routes/web.php
+++ routes/web.php
@@ -9,3 +9,10 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+// Route::get('/profile', 'ProfileController@index')->name('profile');
+// Route::put('/profile', 'ProfileController@update')->name('profile.update');
+
+// Route::get('/about', function () {
+//     return view('about');
+// })->name('about');
