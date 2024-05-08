fc91659cee0b100f9554caef9458f9135c0eb112
Fiko Aditama <fiko@simise.id>
2024-05-08 16:43:44 +0700

    21 - Sistem Infomasi Akuntansi dengan Laravel 10 - Integrasi Datatable

--- gitpush.sh
+++ gitpush.sh
@@ -1,6 +1,6 @@
 git init .
 git add .
-git commit -m "18 - Sistem Infomasi Akuntansi dengan Laravel 10 - Form Master User"
+git commit -m "21 - Sistem Infomasi Akuntansi dengan Laravel 10 - Integrasi Datatable"
 git branch -M main
 git remote add origin1 git@github.com:fico-aditama/si_laravel.git
 git push -u origin main
